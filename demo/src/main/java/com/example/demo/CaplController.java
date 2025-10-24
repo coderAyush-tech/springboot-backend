bd459125a2ea36934587cab419b0e5f9dc0e9263
ayush <sv8317713@gmail.com>
2025-10-24 13:23:13 +0530

    fourth  commit

--- demo/src/main/java/com/example/demo/CaplController.java
+++ demo/src/main/java/com/example/demo/CaplController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-//@CrossOrigin(origins = "*", maxAge = 3600) this is for local
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @CrossOrigin(origins = "https://fullstackkin.netlify.app/")
 public class CaplController {
 
